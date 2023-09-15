ac31fa51575149955536aa4aa6c3abc7b156c051
Meskar Amine <141588867+AMeskar@users.noreply.github.com>
2023-09-15 13:34:22 +0100

    Update RunAll.sh

--- RunAll.sh
+++ RunAll.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+directory="Files"  # Replace with the desired directory path
+
+# Check if the directory exists
+if [ -d "$directory" ]; then
+    # List all files in the directory and print their names
+    for file in "$directory"/*; do
+        if [ -f "$file" ]; then		
+            	sbatch -t 3-00:30 -n 1 --mem 10G  readtxt.sh $file
+        fi
+    done
+else
+    echo "Directory not found."
+fi
+
+echo $1
