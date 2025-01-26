2e83c7cb69fa32774147677efc8ff7b2c1e9dff8
Meskar Amine <141588867+AMeskar@users.noreply.github.com>
2025-01-26 23:01:50 +0100

    Update and rename RunAll.sh to Auto_Run_All.sh

--- Auto_Run_All.sh
+++ Auto_Run_All.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-directory="Files"  # Replace with the desired directory path
+directory="Files"  
 
-# Check if the directory exists
 if [ -d "$directory" ]; then
-    # List all files in the directory and print their names
+
     for file in "$directory"/*; do
         if [ -f "$file" ]; then		
             	sbatch -t 3-00:30 -n 1 --mem 10G  readtxt.sh $file
